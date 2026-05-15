ef0b363aabb7d47fc28219cf27658ba200528832
Pase <62558948+lingmee@users.noreply.github.com>
2026-05-15 16:30:08 +0300

    Delete Pyles Scripts/vscode tutorial directory

--- Pyles Scripts/vscode tutorial/classes_Hello Vscode.lua
+++
@@ -1,9 +0,0 @@
-dofile('C:/REAPER/Scripts/Pyles Scripts/mDebug/mdebug.lua')
-
-local track = reaper.GetTrack(0, 0)
-reaper.ShowConsoleMsg('Hello gamers!')
-
-local t = {track, track, 10, 20, 30, {50,60,70,{80,90,100}}}
-if true then
-    reaper.ShowConsoleMsg('Finishing Script!')
-end
