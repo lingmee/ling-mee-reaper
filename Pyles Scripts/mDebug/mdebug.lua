e243356c46b8b84b28964d33de86733ebeac0947
pase <62558948+lingmee@users.noreply.github.com>
2026-05-16 03:19:02 +0300

    Delete Debug, add env overlay V2 OG
    deleted debug.lua and added another version of the OG script.

--- Pyles Scripts/mDebug/mdebug.lua
+++
@@ -1,4 +0,0 @@
-package.cpath = package.cpath .. ";" .. reaper.GetResourcePath() ..'/Scripts/Classes Mobdebug/socket module/?.dll'    -- WINDOWS ONLY: Add socket module path for .dll files
-package.path = package.path .. ";" .. reaper.GetResourcePath()   ..'/Scripts/Classes Mobdebug/socket module/?.lua'      -- Add all lua socket modules to the path  
-
-require("mobdebug").start() -- Start mobdebug module
